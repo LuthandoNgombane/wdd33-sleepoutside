f28db2c39895933acaa5ac8a141fd94dea4b7f7e
luthandongombane <luthandongombane@gmail.com>
2026-03-29 00:36:58 +0200

    Remove redundant image path cleaning in renderProductDetails function

--- src/js/product.js
+++ src/js/product.js
@@ -20,10 +20,6 @@ async function init() {
 function renderProductDetails(product) {
   document.getElementById("productBrandName").innerText = product.Brand.Name;
   document.getElementById("productName").innerText = product.NameWithoutBrand;
-
-  const cleanPath = product.Image.replace("../", "/"); 
-  document.getElementById("productImage").src = cleanPath;
-
   document.getElementById("productImage").src = product.Image; 
   document.getElementById("productImage").alt = product.Name;
   document.getElementById("productFinalPrice").innerText = `$${product.FinalPrice}`;
